67a12cb7812334e81b5bffc41e015ef42cc09841
Jeongjin Oh <jeongjin.dev@elicer.com>
2025-05-09 02:55:45 +0900

    feat: reproduce resolvePermissions issue

--- puck.config.tsx
+++ puck.config.tsx
@@ -13,6 +13,17 @@ export const config: Config<Props> = {
       defaultProps: {
         title: "Heading",
       },
+      // NOTE
+      // if commented out, the permissions are applied
+      // but if using resolvePermissions with `insert` option, the permissions are not applied
+      // permissions: {
+      //   insert: false,
+      // },
+      resolvePermissions: async (data, params) => {
+        return {
+          insert: false,
+        };
+      },
       render: ({ title }) => (
         <div style={{ padding: 64 }}>
           <h1>{title}</h1>
